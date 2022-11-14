52e7495940a77a844d1bb4e9041c740d20b978b8
simon.vonroenn <simon.vonroenn@web.de>
2022-11-14 11:38:45 +0100

    Begin implementation of bridge subroutine

--- subroutine/bridge.py
+++ subroutine/bridge.py
@@ -6,3 +6,24 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+def main(ev3, mLeft, mRight, sColor):
+
+    robot = DriveBase(mLeft, mRight, wheel_diameter=55.5, axle_track=104)
+    # Set the drive speed at 100 millimeters per second.
+    DRIVE_SPEED = 100
+
+    # Set the gain of the proportional line controller. This means that for every
+    # percentage point of light deviating from the threshold, we set the turn
+    # rate of the drivebase to 1.2 degrees per second.
+
+    # For example, if the light value deviates from the threshold by 10, the robot
+    # steers at 10*1.2 = 12 degrees per second.
+    PROPORTIONAL_GAIN = 1.2
+
+    while True:
+        if sColor.color() == None:
+            robot.straight(-500)
+            robot.turn(90)
+        robot.straight(10)
