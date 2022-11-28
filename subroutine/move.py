0995e64b8276e7541aa49250c4ac92ca0ffa9dee
simon.vonroenn <simon.vonroenn@web.de>
2022-11-28 13:23:19 +0100

    Finish implementation of driveToWall, wallTurn and findBox and begin implementation of moveBoxToWall

--- subroutine/move.py
+++ subroutine/move.py
@@ -7,44 +7,41 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-def wallTurn(robot, DRIVE_SPEED):
+def driveToWall(ev3, robot):
+    adjusted = False
+    while not (sTRight.pressed() and sTLeft.pressed()):
+        distToCenter = sUltra.distance() - 200
+        if abs(distToCenter) < 20: adjusted = True
+        if not adjusted:
+            robot.stop()
+            ev3.screen.print(distToCenter)
+            robot.turn(distToCenter / 3)
+            robot.straight(200)
+            robot.turn( - (distToCenter / 3))
+        else:
+            robot.drive(DRIVE_SPEED, 0)
+
+def wallTurn(robot):
     robot.stop()
     robot.straight(-100)
     robot.turn(580)
 
-def findBox(robot, sUltra, sTRight, sTLeft, DRIVE_SPEED):
+def findBox(robot, sUltra, DRIVE_SPEED):
     robot.drive(DRIVE_SPEED, 0)
     while True:
-        if sUltra.distance < 500:
+        if sUltra.distance() < 500:
             robot.stop()
+
+def moveBoxToWall(robot):
+    robot.turn(290)
+    robot.straight(1000)
+    # to be implemented
 
 def move_main(ev3, mLeft, mRight, mSensor, sColor, sUltra, sTRight, sTLeft):
     robot = DriveBase(mLeft, mRight, wheel_diameter=55.5, axle_track=104)
-    DRIVE_SPEED = 300
+    DRIVE_SPEED = 400
     robot.settings(DRIVE_SPEED, 200, 200, 200)
-    dir = "n"
-    #robot.drive(DRIVE_SPEED, 0)
-    while not (sTRight.pressed() and sTLeft.pressed()):
-        ev3.screen.print(sUltra.distance())
-        distToCenter = sUltra.distance() - 200
-        robot.turn(distToCenter / 3)
-        robot.straight(400 - distToCenter)
-        robot.turn( - (distToCenter / 3))
+    driveToWall(ev3, robot)
     wallTurn(robot, DRIVE_SPEED)
     findBox(robot, sUltra, sTRight, sTLeft, DRIVE_SPEED)
-
-    
-    # if dir != "c" and sUltra.distance() < 250:
-    #         ev3.screen.print("too close")
-    #         dir = "c"
-    #         robot.drive(DRIVE_SPEED, -20)
-    #         robot.reset()
-    #     if dir != "f" and sUltra.distance() > 250:
-    #         ev3.screen.print("too far")
-    #         dir = "f"
-    #         robot.drive(DRIVE_SPEED, 20)
-    #         robot.reset()
-    #     if robot.angle() > 50:
-    #         if dir == "f": robot.turn(-50)
-    #         if dir == "c": robot.turn(50)
-    #         robot.drive(DRIVE_SPEED, 0)
+    moveBoxToWall(robot)
