5f1a477bcf2e7283f73dd0c9df717cb2455138be
simon.vonroenn <simon.vonroenn@web.de>
2022-12-05 09:32:05 +0100

    Add orientate variable which orientates the robot in case the robot doesnt start the subroutine parallelly aligned to the wall

--- subroutine/move.py
+++ subroutine/move.py
@@ -7,19 +7,23 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-def driveToWall(ev3, robot):
+def driveToWall(ev3, robot, sTRight, sTLeft, sUltra):
     adjusted = False
     while not (sTRight.pressed() and sTLeft.pressed()):
-        distToCenter = sUltra.distance() - 200
+        prevDist = sUltra.distance() - 200
+        distToCenter = prevDist
         if abs(distToCenter) < 20: adjusted = True
         if not adjusted:
             robot.stop()
             ev3.screen.print(distToCenter)
-            robot.turn(distToCenter / 3)
+            robot.turn(distToCenter / 2 + orientate)
             robot.straight(200)
-            robot.turn( - (distToCenter / 3))
+            robot.turn( - (distToCenter / 2))
         else:
             robot.drive(DRIVE_SPEED, 0)
+        distToCenter = sUltra.distance() - 200
+        orientate = distToCenter - prevDist + 20
+        
 
 def wallTurn(robot):
     robot.stop()
@@ -41,7 +45,7 @@ def move_main(ev3, mLeft, mRight, mSensor, sColor, sUltra, sTRight, sTLeft):
     robot = DriveBase(mLeft, mRight, wheel_diameter=55.5, axle_track=104)
     DRIVE_SPEED = 400
     robot.settings(DRIVE_SPEED, 200, 200, 200)
-    driveToWall(ev3, robot)
+    driveToWall(ev3, robot, sTRight, sTLeft, sUltra)
     wallTurn(robot, DRIVE_SPEED)
     findBox(robot, sUltra, sTRight, sTLeft, DRIVE_SPEED)
     moveBoxToWall(robot)
