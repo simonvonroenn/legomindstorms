1eb937d5400c364c175ec678c44464a64c343e46
simon.vonroenn <simon.vonroenn@web.de>
2022-12-05 11:24:47 +0100

    Remove orientate variable and add orientate function which orientates the robot at the beginning of the subroutine

--- subroutine/move.py
+++ subroutine/move.py
@@ -7,27 +7,36 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
+def orientate(ev3, robot, sUltra):
+    ANGLE_OFFSET = -60
+    dist = sUltra.distance()
+    while True:
+        dist = sUltra.distance() if sUltra.distance() < dist else dist
+        robot.turn(5)
+        if sUltra.distance() - dist > 5: break
+    while True:
+        dist = sUltra.distance() if sUltra.distance() < dist else dist
+        robot.turn(-5)
+        if sUltra.distance() - dist > 5: break
+    robot.turn(ANGLE_OFFSET)
+
 def driveToWall(ev3, robot, sTRight, sTLeft, sUltra):
-    CENTER_DISTANCE = 200
+    CENTER_DISTANCE = 60
     DRIVE_UNIT = 200
     ADJUSTMENT_PER_UNIT = 20
     ADJUSTMENT_PRECISION = 3
     LEVEL_OF_SIGNIFICANCE = 20
     adjusted = False
-    orientate = 0
     while not (sTRight.pressed() and sTLeft.pressed()):
         prevDist = sUltra.distance() - CENTER_DISTANCE
         distToCenter = prevDist
         if abs(distToCenter) < LEVEL_OF_SIGNIFICANCE: adjusted = True
         if not adjusted:
             robot.stop()
-            ev3.screen.print("distToCenter: " + str(distToCenter))
+            ev3.screen.print(distToCenter)
             robot.turn(distToCenter / ADJUSTMENT_PRECISION + orientate)
             robot.straight(DRIVE_UNIT)
             robot.turn( - (distToCenter / ADJUSTMENT_PRECISION))
-            distToCenter = sUltra.distance() - CENTER_DISTANCE
-            orientate = distToCenter - prevDist + (ADJUSTMENT_PER_UNIT if distToCenter > CENTER_DISTANCE else -ADJUSTMENT_PER_UNIT)
-            ev3.screen.print("diff: " + str(orientate))
         else:
             robot.drive(DRIVE_SPEED, 0)
         
@@ -52,7 +61,10 @@ def move_main(ev3, mLeft, mRight, mSensor, sColor, sUltra, sTRight, sTLeft):
     robot = DriveBase(mLeft, mRight, wheel_diameter=55.5, axle_track=104)
     DRIVE_SPEED = 400
     robot.settings(DRIVE_SPEED, 200, 200, 200)
-    driveToWall(ev3, robot, sTRight, sTLeft, sUltra)
-    wallTurn(robot, DRIVE_SPEED)
-    findBox(robot, sUltra, sTRight, sTLeft, DRIVE_SPEED)
-    moveBoxToWall(robot)
+    # while True:
+    #     ev3.screen.print(sUltra.distance())
+    orientate(ev3, robot, sUltra)
+    # driveToWall(ev3, robot, sTRight, sTLeft, sUltra)
+    # wallTurn(robot, DRIVE_SPEED)
+    # findBox(robot, sUltra, sTRight, sTLeft, DRIVE_SPEED)
+    # moveBoxToWall(robot)
