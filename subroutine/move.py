2bfdaaad01cb60e44f57c543c0bb2745e2fd84a8
simon.vonroenn <simon.vonroenn@web.de>
2022-12-12 13:02:56 +0100

    Hide ultrasonic sensor while driving towards the wall and adjust some parameters

--- subroutine/move.py
+++ subroutine/move.py
@@ -22,39 +22,42 @@ import time
 #         if sUltra.distance() - dist > 0: break
 #     robot.turn(ANGLE_OFFSET)
 
-def driveToWall(ev3, robot, sTRight, sTLeft, sUltra, DRIVE_SPEED):
-    CENTER_DISTANCE = 30
-    DRIVE_UNIT = 200
-    ADJUSTMENT_PRECISION = 2.5
-    TOLERANCE = 30
-    adjusted = False
-    while not (sTRight.pressed() or sTLeft.pressed()):
+def driveToWall(ev3, robot, sTRight, sTLeft, sUltra, mSensor, DRIVE_SPEED):
+    ev3.screen.print("driveToWall")
+    CENTER_DISTANCE = 60
+    DRIVE_UNIT = 80
+    ADJUSTMENT_PRECISION = 3
+    TOLERANCE = 10
+    while True:
         prevDist = sUltra.distance() - CENTER_DISTANCE
         distToCenter = prevDist
-        if abs(distToCenter) < TOLERANCE: adjusted = True
-        if not adjusted:
-            robot.stop()
-            ev3.screen.print(distToCenter)
-            robot.turn(distToCenter / ADJUSTMENT_PRECISION)
-            robot.straight(DRIVE_UNIT)
-            robot.turn( - (distToCenter / ADJUSTMENT_PRECISION))
-        else:
-            robot.drive(DRIVE_SPEED, 0)
+        if abs(distToCenter) < TOLERANCE: break
+        robot.turn(distToCenter / ADJUSTMENT_PRECISION)
+        robot.straight(DRIVE_UNIT)
+        robot.turn( - (distToCenter / ADJUSTMENT_PRECISION))
     
-def wallTurn(robot):
+    mSensor.run_target(100, 90)
+    while not (sTRight.pressed() or sTLeft.pressed()):
+        robot.drive(DRIVE_SPEED, 0)
+    
+def wallTurn(ev3, robot):
+    ev3.screen.print("wallTurn")
     robot.stop()
     robot.straight(-100)
     robot.turn(-180)
 
-def findBox(robot, sUltra, DRIVE_SPEED):
+def findBox(ev3, robot, sUltra, mSensor, DRIVE_SPEED):
+    ev3.screen.print("findBox")
+    mSensor.run_target(100, 0)
     robot.drive(DRIVE_SPEED, 0)
     while True:
         if sUltra.distance() < 500:
             robot.stop()
-            robot.straight(50)
+            robot.straight(100)
             break
 
-def moveBoxToWall(robot, DRIVE_SPEED):
+def moveBoxToWall(ev3, robot, DRIVE_SPEED):
+    ev3.screen.print("moveBoxToWall")
     robot.turn(90)
     robot.reset()
     robot.drive(DRIVE_SPEED, 0)
@@ -62,11 +65,14 @@ def moveBoxToWall(robot, DRIVE_SPEED):
         robot.reset()
         time.sleep(1)
     robot.stop()
-    robot.straight(-100)
+
+def moveBoxToCorner(ev3, robot, DRIVE_SPEED):
+    ev3.screen.print("moveBoxToCorner")
+    robot.straight(-50)
     robot.turn(-90)
-    robot.straight(250)
+    robot.straight(150)
     robot.turn(90)
-    robot.straight(200)
+    robot.straight(100)
     robot.turn(90)
     robot.drive(DRIVE_SPEED, 0)
     while robot.distance() < 5:
@@ -75,13 +81,13 @@ def moveBoxToWall(robot, DRIVE_SPEED):
 
 def move_main(ev3, mLeft, mRight, mSensor, sColor, sUltra, sTRight, sTLeft):
     robot = DriveBase(mLeft, mRight, wheel_diameter=43, axle_track=130)
-    DRIVE_SPEED = 100
+    DRIVE_SPEED = 200
     robot.settings(DRIVE_SPEED, 200, 200, 200)
     # while True:
     #     ev3.screen.print(sUltra.distance())
     # orientate(robot, sUltra)
-    driveToWall(ev3, robot, sTRight, sTLeft, sUltra, DRIVE_SPEED)
-    wallTurn(robot)
-    mSensor.run_target(50, -180)
-    findBox(robot, sUltra, DRIVE_SPEED)
-    moveBoxToWall(robot, DRIVE_SPEED)
+    driveToWall(ev3, robot, sTRight, sTLeft, sUltra, mSensor, DRIVE_SPEED)
+    wallTurn(ev3, robot)
+    findBox(ev3, robot, sUltra, mSensor, DRIVE_SPEED)
+    moveBoxToWall(ev3, robot, DRIVE_SPEED)
+    moveBoxToCorner(ev3, robot, DRIVE_SPEED)
