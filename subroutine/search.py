edd7b93ca1372d0efc3ef9edcb4eeb005798becb
JackN4pier <adrian.nelson.x@gmail.com>
2022-12-19 09:55:00 +0100

    search strategy

--- subroutine/search.py
+++ subroutine/search.py
@@ -6,3 +6,11 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+def search_marker():
+    #enters on right side of wall
+    #optimal path: up; 90 deg left, short burst, 90 deg left; down; 90 deg right, short burst, 90 deg right;
+    #for up and down use drive and continually check for press of button and marker
+    #for short burst use straight and two checks for the maerker
+    #do for (measure how often)
